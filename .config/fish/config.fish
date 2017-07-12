3008f7f903aa82f949f2667200f33b75ca78e2be
nemusou <ryubi.102395@gmail.com>
2017-07-12 17:18:59 +0900

    add anyenv, pyenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 . /home/nemu_sou/.config/fish/private.fish
+. /home/nemu_sou/.config/fish/functions/fish_prompt.fish
 
 # alias {{{
 alias home "cd /mnt/c/Users/$USER_NAME"
@@ -19,3 +20,34 @@ end
 
 # }}}
 
+# anyenv {{{
+set --export PATH  "$HOME/.anyenv/bin" $PATH
+source "/home/nemu_sou/.anyenv/libexec/../completions/anyenv.fish"
+function anyenv
+  set command $argv[1]
+  set -e argv[1]
+
+  command anyenv "$command" $argv
+end
+
+# pyenv
+set -x PYENV_ROOT "$HOME/.anyenv/envs/pyenv"
+set -x PATH $PATH "$HOME/.anyenv/envs/pyenv/bin"
+# pyenv init - fish
+set -gx PATH '/home/nemu_sou/.anyenv/envs/pyenv/shims' $PATH
+set -gx PYENV_SHELL fish
+source '/home/nemu_sou/.anyenv/envs/pyenv/libexec/../completions/pyenv.fish'
+command pyenv rehash 2>/dev/null
+function pyenv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case rehash shell
+    source (pyenv "sh-$command" $argv|psub)
+  case '*'
+    command pyenv "$command" $argv
+  end
+end
+
+# }}}
