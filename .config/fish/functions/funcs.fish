c6d450e4905c31482f5c993d50135b0d57ed7447
nemusou <ryubi.102395@gmail.com>
2017-07-22 17:43:11 +0900

    fix if condition

--- .config/fish/functions/funcs.fish
+++ .config/fish/functions/funcs.fish
@@ -1,11 +1,12 @@
 # google-chrome
 function chrome
   set -l url
-  if [ -n "$argv[1]" ]
+  if [ "$argv[1]" = "" ]
     set url "google.com"
   else
     set url $argv[1]
   end
+  echo $url
   /mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe $url
 end
 
