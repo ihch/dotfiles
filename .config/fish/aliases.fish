c51c3a87e80fd7e69736f4afedd94a1216cb7b79
nemusou <ryubi.102395@gmail.com>
2017-07-30 16:17:13 +0900

    add clip.exe How to use cat filename | clip

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,4 +1,8 @@
 alias home "cd /mnt/c/Users/$USER_NAME"
 alias desktop "cd /mnt/c/Users/$USER_NAME/Desktop"
+alias doc "cd /mnt/c/Users/$USER_NAME/Documents"
 alias vim "vim -p"
 alias vim "nvim"
+
+# clipboard
+alias clip "/mnt/c/Windows/System32/clip.exe"
